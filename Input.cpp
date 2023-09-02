583b826908e292cf4ff36ce95fbf68967b2fbe9b
atishayjain2003 <atishayjain2003@gmail.com>
2023-09-02 09:42:33 +0530

    Taking the user input

---
+++ Input.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int marks;
+    cout<<"Please enter the marks"<<endl;
+    cin>>marks;
+    cout<<"The entered marks are";
+    cout<<"\n";
+    cout<<marks;
+
+
+}
