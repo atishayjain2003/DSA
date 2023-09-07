311585625bdcba766d26784ae850a0b1bcc63abd
atishayjain2003 <atishayjain2003@gmail.com>
2023-09-07 20:25:27 +0530

    Shift operations including left and right

---
+++ shift.cpp
@@ -0,0 +1,22 @@
+/*#include<iostream>
+using namespace std;
+int main()
+{
+    int x,y;
+    cout<<"Enter number";
+    cin>>x;
+    y=x << 3;
+    cout<<y;
+
+}
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    int number;
+    cout<<"Enter number";
+    cin>>number;
+    cout<< (number>>2);
+
+}
