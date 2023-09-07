6ffbc12389cabf88368601e18d8c0d44ca627251
atishayjain2003 <atishayjain2003@gmail.com>
2023-09-07 19:24:23 +0530

    Performing Bitwise Operations

---
+++ Operations.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x,y,z;
+
+    cout<<"Enter numbers";
+    cin>>x>>y;
+    cout<<(x ^ y);
+    //cout<<"Enter number";
+    //cin>>z;
+    //cout<<~z;
+
+
+
+}
