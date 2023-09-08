64bb411b2d8ee37267e70ca6aec29dbdcb8a07fd
atishayjain2003 <atishayjain2003@gmail.com>
2023-09-08 13:21:19 +0530

    Use of return statement in function

--- function2.cpp
+++ function2.cpp
@@ -2,12 +2,13 @@
 using namespace std;
 int add(int a, int b)
 {
-    cout<<"The required sum is "<<a+b;
+    int sum=a+b;
+    return sum;
 }
 int main()
 {
     int x,y;
     cout<<"Enter numbers to be addedd";
     cin>>x>>y;
-    add(x,y);
+    cout<<add(x,y);
 }
