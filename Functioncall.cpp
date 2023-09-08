c5911faa353dc81b68a5223ff663220a7d4e7ca0
atishayjain2003 <atishayjain2003@gmail.com>
2023-09-08 13:40:23 +0530

    Understanding function call

---
+++ Functioncall.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+void printa()
+{
+    cout<<"Inside A"<<endl;
+    cout<<"Hello"<<endl;
+}
+int main()
+{
+    printa();
+    cout<<"The execution has ended";
+}
+
+//the expected output is
+/* Inside a
+Hello
+The execution has ended 
+*/
