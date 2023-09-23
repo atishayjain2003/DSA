643a2ceb04c71e67c0a0d22c85f316bc833d3e99
atishayjain2003 <atishayjain2003@gmail.com>
2023-09-24 00:36:58 +0530

    Type conversion

---
+++ Typecasting.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    /*
+    int a=65;
+    char b=a+1;
+    cout<<b;
+    
+    int num1=10;
+    float num2=5.5;
+    int sum=num1+num2;
+    cout<<float(sum);
+    
+
+    float number=67.33;
+    cout<<char(number);
+    */
+
+    int dividend=10;
+    float divisor=3.0;
+    float quotient=dividend/divisor;
+    cout<<quotient;
+}
