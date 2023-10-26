9678c4d5cc7f6272c9a57eb11fd27c040a79c97f
atishayjain2003 <atishayjain2003@gmail.com>
2023-10-27 00:35:09 +0530

    Leetcode Problem solution

---
+++ Row with maximum ones/Leetcodeprobelm.cpp
@@ -0,0 +1,35 @@
+// Finding the row with maximum ones 
+
+class Solution {
+public:
+    vector<int> rowAndMaximumOnes(vector<vector<int>>& mat) {
+        vector<int> ans;
+        int n = mat.size();
+        //oneCOunt -> will store max number of 1's in a row
+        int oneCount = INT_MIN;
+        //rowNo -> will store index of max no. of 1's wali row
+        int rowNo ;
+
+        for(int i=0; i<n; i++) {
+            //hr row start hone se phle initialise count with 0
+            int count = 0;
+            for(int j=0; j< mat[i].size(); j++) {
+                //if 1 found, then increment count
+                if(mat[i][j] == 1) {
+                    count++;
+                }
+            }
+            //after row completion, compare count with oneCount
+            if(count > oneCount) {
+                oneCount = count;
+                rowNo = i;
+            }
+
+        }
+
+        ans.push_back(rowNo);
+        ans.push_back(oneCount);
+        return ans;
+        
+    }
+};
