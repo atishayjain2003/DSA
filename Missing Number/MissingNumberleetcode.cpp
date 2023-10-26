83523580640477190e7e8d451a738f597c5941bf
atishayjain2003 <atishayjain2003@gmail.com>
2023-10-26 23:48:07 +0530

    Missing Number Problem

---
+++ Missing Number/MissingNumberleetcode.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int sum=0;
+        int n=nums.size();
+        for(int i=0; i<n; i++)
+        {
+            sum+=nums[i];
+        }
+        int absolute=(n*(n+1))/2;
+        int output=absolute-sum;
+        return output;
+    }
+};
