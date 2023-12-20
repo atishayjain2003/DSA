2597b3be6852f1e775ead55b25d5a7e208e09944
atishayjain2003 <atishayjain2003@gmail.com>
2023-12-20 22:26:13 +0530

    Leetcode Solution

---
+++ Buy two chocolates/Leetcode.cpp
@@ -0,0 +1,26 @@
+/*You are given an integer array prices representing the prices of various chocolates in a store. You are also given a single integer money, which represents your initial amount of money.
+
+You must buy exactly two chocolates in such a way that you still have some non-negative leftover money. You would like to minimize the sum of the prices of the two chocolates you buy.
+
+Return the amount of money you will have leftover after buying the two chocolates. If there is no way for you to buy two chocolates without ending up in debt, return money. Note that the leftover must be non-negative.
+*/
+//Approach: Minimum sum= sum of first two elements ,if sum is greater than money then return money else return money-sum
+
+//Solution
+class Solution {
+public:
+    int buyChoco(vector<int>& prices, int money) {
+        int x=prices.size();
+        sort(prices.begin(),prices.end());
+        int sum=prices[0]+prices[1];
+        if (sum>money)
+        {
+            return money;
+        }
+        else
+        {
+            return money-sum;
+        }
+        
+    }
+};
