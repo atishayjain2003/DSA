6c9a81feaa2289a8aab12623071c69b34d9f9f44
atishayjain2003 <atishayjain2003@gmail.com>
2024-02-20 18:04:58 +0530

    Insertion at head

--- Linked List/insertathead.cpp
+++ Linked List/insertathead.cpp
@@ -17,25 +17,22 @@ int main()
     node* first= new node(10);
     node* second= new node(20);
     node* third= new node(30);
-    first->next=second;
-    second->next=third;
-    node* attail= new node(50);
     node* head=first;
     node* temp=head;
-    node* tail=head;
-    while(tail->next!=NULL)
-    {
-        tail=tail->next;
-    }
-    tail->next=attail;
-
-    cout<<"The linked list is "<< endl;
+    first->next=second;
+    second->next=third;
+    node* athead=new node(50);
+    athead->next=head;
+    head=athead;
+    temp=head;
+    cout<<"The linked list after insertion at head is "<< endl;
     while(temp!=NULL)
     {
-        cout<<temp->data<<" ";
+        cout<<temp->data<<"->";
         temp=temp->next;
     }
-    //insert at tail completed
-     
+
+
+    
 
 }
