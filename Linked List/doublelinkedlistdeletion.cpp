4670d503b12f708c161799d925824585d2413bc4
atishayjain2003 <atishayjain2003@gmail.com>
2024-02-27 05:29:56 +0530

    Deletion at head from double linked list

---
+++ Linked List/doublelinkedlistdeletion.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+class node
+{
+    public:
+    int data;
+    node* prev;
+    node* next;
+    node(int data)
+    {
+        this->data=data;
+        this->prev=NULL;
+        this->next=NULL;
+    }
+};
+int main()
+{
+    node* first= new node(10);
+    node* second=new node(20);
+    node* third= new node(30);
+    node* fourth= new node(40);
+    first->next=second;
+    second->next=third;
+    third->next=fourth;
+    second->prev=first;
+    third->prev=second;
+    fourth->prev=third;
+    node* head=first;
+    node* temp=head;
+    //delete from head
+    head=temp->next;
+    temp->next=NULL;
+    delete temp;
+    head->prev=NULL;
+    temp=head;
+    while(temp!=NULL)
+    {
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+
+
+}
