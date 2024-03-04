8d7dd3db24b77ecd9fa12430cb65a0bf7ecf6878
atishayjain2003 <atishayjain2003@gmail.com>
2024-03-05 00:47:38 +0530

    Sorting a stack

--- Linked List/Stack/practice1.cpp
+++ Linked List/Stack/practice1.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
 #include<stack>
+#include<vector>
+#include<bits/stdc++.h>
 using namespace std;
 int main()
 {
@@ -21,6 +23,7 @@ int main()
         st.pop();
     }*/
     //string reversal using stack 
+    /*
     string name="ARORA";
     int x=name.length();
     stack<char>value;
@@ -37,7 +40,7 @@ int main()
     }
 
     //cout<<"The reversed string is "<<result;
-
+    /*
     if(result==name)
     {
         cout<<"Palindrome";
@@ -45,6 +48,37 @@ int main()
     else{
         cout<<"Not a palindrome";
     }
+    */
+   //sort a stack
+   stack<int>st1;
+   stack<int>st2;
+   st1.push(14);
+   st1.push(8);
+   st1.push(97);
+   st1.push(2);
+   st1.push(45);
+   int x=st1.size();
+
+   vector<int>result;
+   for(int i=0; i<x; i++)
+   {
+    result.push_back(st1.top());
+    st1.pop();
+   }
+   sort(result.begin(), result.end());
+   for(int i=0; i<x; i++)
+   {
+    st2.push(result[i]);
+   }
+
+   cout<<"The sorted stack in descending order is "<< endl;
+   for(int i=0; i<x; i++)
+   {
+    cout<<st2.top()<<" ";
+    st2.pop();
+   }
+
+
 
 
     
