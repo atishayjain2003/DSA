f99cdfd9a92f1dbf1b41ca92be9485fa6cbebe46
Atishay Jain <74485440+atishayjain2003@users.noreply.github.com>
2024-04-24 23:23:39 +0530

    Time: 0 ms (100.00%) | Memory: 7.2 MB (29.12%) - LeetSync

---
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+       if(n==0){
+        return 0;
+       }
+        if(n==1){
+        return 1;
+       }
+        if(n==2){
+        return 1;
+       }
+       int a=0;
+       int b=1;
+       int c=1;
+       int temp=0;
+        for(int i=3;i<=n;i++){
+            temp=a+b+c;
+            a=b;
+            b=c;
+            c=temp;
+            temp=0;
+        }
+        return c;
+    }
+};
