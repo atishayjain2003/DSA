7c6d1f6af3c7aedfbbd09cbc3978cd31656614c9
atishayjain2003 <atishayjain2003@gmail.com>
2024-09-04 23:11:49 +0530

    Graph Representation in the form of adjacency matrix

---
+++ GRAPHS/adjacencymatrix.cpp
@@ -0,0 +1,33 @@
+//for undirected graph
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+int main()
+{
+    int nodes;
+    cout<<"Enter number of nodes ";
+    cin>>nodes;
+    int matrix[nodes+1][nodes+1]={0};
+    for(int i=1; i<nodes+1; i++)
+    {
+        int source;
+        int destination; 
+        cout<<"Enter source and destination ";
+        cin>>source>>destination ;
+        matrix[source][destination]=1;
+        matrix[destination][source]=1;
+        
+    }
+
+    for(int i=1; i<nodes+1; i++)
+    {
+        for(int j=1; j<nodes+1; j++)
+        {
+            cout<< matrix[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+}
